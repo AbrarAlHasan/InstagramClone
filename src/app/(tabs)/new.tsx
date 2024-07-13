4eb207af12d7c3646ae969d98df41571b7de5ecb
Abrar Al Hasan <abraralhasan111@gmail.com>
2024-07-13 15:03:54 +0530

    Image Picker

--- src/app/(tabs)/new.tsx
+++ src/app/(tabs)/new.tsx
@@ -6,26 +6,59 @@ import {
   TextInput,
   View,
 } from "react-native";
-import React, { useState } from "react";
+import React, { useCallback, useState } from "react";
+import * as ImagePicker from "expo-image-picker";
+import { useFocusEffect } from "expo-router";
 
 const CreatePost = () => {
   const [caption, setCaption] = useState("");
+  const [image, setImage] = useState("");
+
+  useFocusEffect(
+    useCallback(() => {
+      if (!image) {
+        pickImage();
+      }
+      return setImage("");
+    }, [])
+  );
+
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+  };
 
   return (
-    <View className="p-3 items-center  flex-1">
+    <View className=" items-center  flex-1">
       {/* Image Picker */}
-      <Image
-        source={{
-          uri: "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/1.jpg",
+      {image ? (
+        <Image
+          source={{
+            uri: image,
+          }}
+          className="w-full aspect-[4/3] shadow-md bg-gray-300"
+        />
+      ) : (
+        <View className="w-full aspect-[4/3]  bg-gray-300" />
+      )}
+      <Pressable
+        onPress={() => {
+          pickImage();
         }}
-        className="w-52 aspect-[3/4] rounded-lg shadow-md"
-      />
-      <Text
-        className="text-blue-500 font-semibold m-5 text-lg"
-        onPress={() => {}}
       >
-        Change
-      </Text>
+        <Text className="text-blue-500 font-semibold m-5 text-lg">
+          {image ? "Change Image" : "Select Image"}
+        </Text>
+      </Pressable>
       {/* Text Input for Caption */}
       <TextInput
         placeholder="Write a Caption"
@@ -36,7 +69,7 @@ const CreatePost = () => {
         multiline={true}
       />
       {/* Button */}
-      <View className="mt-auto w-full">
+      <View className="mt-auto w-full px-3 ">
         <Pressable className="bg-blue-500 w-full p-3 items-center rounded-md">
           <Text className="text-white font-semibold">Share</Text>
         </Pressable>
