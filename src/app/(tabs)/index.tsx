6448e769f6af446e791d8eafdbdfb18237d740b3
Abrar Al Hasan <abraralhasan111@gmail.com>
2024-07-13 14:25:07 +0530

    Responsive feed Website

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,13 +1,27 @@
-import { FlatList, Image, StyleSheet, Text, View } from "react-native";
+import {
+  FlatList,
+  Image,
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 import React from "react";
 import PostListItem from "@/src/components/PostListItem";
 import posts from "@/assets/data/posts.json";
 
 const FeedScreen = () => {
+  console.log("OSSSS____", Platform.OS);
+  const classNames = Platform.OS === "web" ? "items-center" : "";
   return (
     <FlatList
       data={posts}
-      contentContainerStyle={{ gap: 10, backgroundColor: "white" }}
+      className={classNames}
+      contentContainerStyle={{
+        gap: 10,
+        maxWidth: 512,
+        width: "100%",
+      }}
       renderItem={({ item }) => <PostListItem post={item} />}
       showsVerticalScrollIndicator={false}
     />
